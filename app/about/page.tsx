b3752c519d16cc3a93ae7a686fe0a9ba6e278aeb
yuta-hamasaki <yh02052002@gmail.com>
2024-06-20 21:38:44 -0700

    Added midsection at about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Header from '../components/Header';
 import History from '../components/History';
 import About from '../components/About'
+import MidSection from '../components/MidSection';
 
 const page = () => {
   return (
@@ -11,9 +12,7 @@ const page = () => {
         <div className="mt-20 md:px-36 px-5">
         <About/>
         </div>
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="fill-green-50"></path>
-        </svg>
+        <MidSection/>
         <div className='bg-green-50'>
           <History />
         </div>
